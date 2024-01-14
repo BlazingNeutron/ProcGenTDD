a5cb3ca4cea34e4c5886b6c412a0464d4af64a03
BlazingNeutron <steven.armstrong+github@gmail.com>
2024-01-14 08:45:32 -0500

    Add simple noise function

--- procgentest1.Tests/ProcGenTests.cs
+++ procgentest1.Tests/ProcGenTests.cs
@@ -4,22 +4,33 @@ using procgentest1;
 
 public class ProcGenTests
 {
+    private const int LENGTH_ONE_FLOOR_SEED = 1;
+    private const int LENGTH_TWO_FLOOR_SEED = 9;
+    private const int LENGTH_TWO_FLOOR_JUMP_SEED = 2;
+
     ProcGenLevel levelGenerator = new ProcGenLevel();
 
     [Fact]
     public void SimplestLevel()
     {
-        Assert.Equal(levelGenerator.Generate(0), ["S", "E"]);
+        Assert.Equal(["S", "E"], levelGenerator.Generate(0));
     }
 
     [Fact]
     public void AddOneFloor() {
-        Assert.Equal(levelGenerator.Generate(1), ["S", "F", "E" ]);
+        levelGenerator.SetSeed(LENGTH_ONE_FLOOR_SEED);
+        Assert.Equal(["S", "F", "E" ], levelGenerator.Generate(1));
     }
 
     [Fact]
     public void RandomMaxLengthAllFloor() {
-        levelGenerator.SetSeed(122);
-        Assert.Equal(levelGenerator.GenerateRandom(5), ["S", "F", "F", "E" ]);
+        levelGenerator.SetSeed(LENGTH_TWO_FLOOR_SEED);
+        Assert.Equal(["S", "F", "F", "E" ], levelGenerator.GenerateRandom(5));
+    }
+
+    [Fact]
+    public void LevelWithAJump() {
+        levelGenerator.SetSeed(LENGTH_TWO_FLOOR_JUMP_SEED);
+        Assert.Equal(["S", "F", "F", "", "E" ], levelGenerator.GenerateRandom(5));
     }
 }
